1cd08b7837a78019a097a38adab1eca15f8711d9
Sinderella <rawsec@gmail.com>
2016-01-17 14:49:40 +0000

    Updated db schema
    Why:
    
    * To ease the process of retrieving and analysing the information

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
-create table documents (
-    id integer primary key autoincrement not null,
-    url text,
-    path text
+CREATE TABLE documents (
+  did INTEGER PRIMARY KEY autoincrement NOT NULL,
+  url text,
+  path text
+);
+
+CREATE TABLE entities (
+  eid INTEGER PRIMARY KEY autoincrement NOT NULL,
+  did INTEGER,
+  type text,
+  entity text,
+  FOREIGN KEY(did) REFERENCES documents(did),
+  FOREIGN KEY(type) REFERENCES entity_type(type)
+);
+
+CREATE TABLE entity_types (
+  type text
 );
---
+++ types.sql
@@ -0,0 +1,10 @@
+INSERT INTO entity_types(type) VALUES
+  ('Name'),
+  ('Email'),
+  ('Image'),
+  ('URL'),
+  ('Education'),
+  ('Job'),
+  ('User ID'),
+  ('Organisation'),
+  ('Location');
