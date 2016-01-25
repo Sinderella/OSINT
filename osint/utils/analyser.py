0458735ecf945bc967bc7a38b962deea0a6dad8f
Sinderella <rawsec@gmail.com>
2016-01-25 00:05:34 +0000

    Added ambguity as a matric to the analysis process
    It will give the score of the ambuguity to a location name and a person's name
    Needed to think more about the other types of the entity

--- osint/utils/analyser.py
+++ osint/utils/analyser.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import sqlite3
+import json
 
 from pandas import DataFrame, Series
+
+from osint.utils.requesters import Requester
 
 
 class Analyser(object):
@@ -12,30 +15,96 @@ class Analyser(object):
         self.cur_db = sqlite3.connect(self.db_name + '/documents.db')
         self.cursor = self.cur_db.cursor()
 
+        self.google_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address={}"
+
+        self.location_score_chart = {
+            'type_10': ['street_address', 'subpremise', 'airport', 'park', 'point_of_interest'],
+            'type_9': ['intersection', 'neighborhood', 'premise', 'postal_code'],
+            'type_8': ['route', 'administrative_area_level_5', 'sublocality_level_5', 'locality', 'ward'],
+            'type_7': ['administrative_area_level_4', 'sublocality_level_4'],
+            'type_6': ['administrative_area_level_3', 'sublocality_level_3'],
+            'type_5': ['political', 'administrative_area_level_2', 'sublocality_level_2'],
+            'type_4': ['administrative_area_level_1', 'sublocality_level_1'],
+            'type_3': ['country']}
+
     def analyse(self):
         print('Frequency:\n{}'.format(self.compute_frequency()))
         print('TF.IDF:\n{}'.format(self.compute_tf_idf()))
+        print('Ambiguity:\n{}'.format(self.compute_ambiguity()))
 
     @staticmethod
-    def get_category_top5(df, sort_by):
+    def get_category_top5(df, sort_by, ascending=False):
         result = None
         for e_type in df['e_type'].unique():
             if result is None:
-                result = df[df['e_type'] == e_type].sort_values(sort_by, ascending=False).iloc[0:5]
+                result = df[df['e_type'] == e_type].sort_values(sort_by, ascending=ascending).iloc[0:5]
             else:
                 result = pd.concat([result, df[df['e_type'] == e_type]
-                                   .sort_values(sort_by, ascending=False).iloc[0:5]])
+                                   .sort_values(sort_by, ascending=ascending).iloc[0:5]])
 
-        return result.sort_values(['e_type', sort_by], ascending=[True, False]).to_string(index=False)
+        return result.sort_values(['e_type', sort_by], ascending=[not ascending, ascending]).to_string(index=False)
+
+    def compute_ambiguity(self):
+        results = self.cursor.execute('SELECT type, entity FROM entities')
+        tmp = results.fetchall()
+        freq_df = DataFrame(tmp, columns=['e_type', 'entity'])
+        freq_df['score'] = np.nan
+        freq_df = freq_df.drop_duplicates()
+        result_computed_location = self._compute_location_ambiguity(freq_df)
+        result_computed_name = self._compute_name_ambiguity(result_computed_location)
+        return self.get_category_top5(result_computed_name, 'score')
+
+    def _compute_location_ambiguity(self, df):
+        location_df = df[df['e_type'] == 'Location']
+        location_se = location_df.drop_duplicates()['entity']
+        for location_name in location_se.get_values():
+            url = self.google_api_url.format(location_name)
+            req = Requester()
+            res = req.get(url)
+            api_result = json.loads(res.content)
+            if api_result['status'] != 'OK':
+                continue
+            location_types = api_result['results'][0]['address_components'][0]['types']
+            location_score = max(self.__measure_location_ambiguity(location_type) for location_type in location_types)
+            location_index = location_se[location_se == location_name].index[0]
+            df.set_value(location_index, 'score', location_score)
+        return df
+
+    def __measure_location_ambiguity(self, location):
+        score = 0
+        for score_type in self.location_score_chart:
+            if location in self.location_score_chart[score_type]:
+                score = int(score_type.split('_')[-1])
+                break
+        return score
+
+    def _compute_name_ambiguity(self, df):
+        name_df = df[df['e_type'] == 'Name']
+        name_se = name_df.drop_duplicates()['entity']
+        for name_name in name_se.get_values():
+            name_score = self.__compute_name_ambiguity(name_name)
+            name_index = name_se[name_se == name_name].index[0]
+            df.set_value(name_index, 'score', name_score)
+        return df
+
+    @staticmethod
+    def __compute_name_ambiguity(name):
+        score = 0
+        if len(name.split(' ')) > 1:
+            score = 1
+        return score
 
     def compute_frequency(self):
+        return self.get_category_top5(self._compute_frequency(), 'term_freq')
+
+    def _compute_frequency(self):
         results = self.cursor.execute('SELECT did, type, entity FROM entities')
         tmp = results.fetchall()
         df = DataFrame(tmp, columns=['did', 'e_type', 'entity'])
         tmp = df.groupby(['e_type', 'entity']).size().reset_index()
         tmp.rename(columns={0: 'term_freq'}, inplace=True)
 
-        return self.get_category_top5(tmp, 'term_freq')
+        return tmp
 
     def compute_tf_idf(self):
         # Find total number of document
