Fixed TF-IDF computation bug and added consistency

osint/utils/analyser.py
@@ -1,3 +1,6 @@
+import codecs
+import os
+
 import pandas as pd
 import numpy as np
 import sqlite3
@@ -9,11 +12,13 @@ from osint.utils.requesters import Requester
 
 
 class Analyser(object):
-    def __init__(self, db_name):
+    def __init__(self, db_name, queries):
         super(Analyser, self).__init__()
         self.db_name = db_name
         self.cur_db = sqlite3.connect(self.db_name + '/documents.db')
         self.cursor = self.cur_db.cursor()
+
+        self.queries = queries
 
         self.google_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address={}"
 
@@ -32,6 +37,7 @@ class Analyser(object):
         combined_df = DataFrame(columns=['e_type', 'entity', 'score'])
 
         frequency_df = self.compute_frequency()
+        consistency_df = self.compute_consistency()
         tf_idf_df = self.compute_tf_idf()
         ambiguity_df = self.compute_ambiguity()
 
@@ -39,16 +45,13 @@ class Analyser(object):
         del combined_df['term_freq']
         combined_df['score'] = 0
         combined_df = self.normalise_and_combine(frequency_df, combined_df)
+        combined_df = self.normalise_and_combine(consistency_df, combined_df)
         combined_df = self.normalise_and_combine(tf_idf_df, combined_df)
         combined_df = self.normalise_and_combine(ambiguity_df, combined_df)
 
-        combined_df['score'] = combined_df['score'].apply(lambda x: x/3)
+        combined_df['score'] = combined_df['score'].apply(lambda x: x / 4)
 
         print(self.get_category_top5(combined_df, 'score'))
-
-        # print('Frequency:\n{}'.format(self.compute_frequency()))
-        # print('TF.IDF:\n{}'.format(self.compute_tf_idf()))
-        # print('Ambiguity:\n{}'.format(self.compute_ambiguity()))
 
     @staticmethod
     def normalise_and_combine(src_df, dst_df):
@@ -57,13 +60,15 @@ class Analyser(object):
         upper_bound = 0
         if 'term_freq' in src_df.columns:
             column = 'term_freq'
+        elif 'term_cons' in src_df.columns:
+            column = 'term_cons'
         elif 'tf_idf' in src_df.columns:
             column = 'tf_idf'
         elif 'score' in src_df.columns:
             column = 'score'
         lower_bound = float(src_df[column].min())
         upper_bound = float(src_df[column].max())
-        src_df[column] = src_df[column].apply(lambda x: (x - lower_bound)/(upper_bound - lower_bound))
+        src_df[column] = src_df[column].apply(lambda x: (x - lower_bound) / (upper_bound - lower_bound))
         src_df.rename(columns={column: 'score'}, inplace=True)
         output = pd.merge(dst_df, src_df, on=["e_type", "entity"])
         output['score'] = output['score_x'] + output['score_y']
@@ -145,49 +150,85 @@ class Analyser(object):
 
         return tmp
 
+    def compute_consistency(self):
+        return self._compute_consistency()
+
+    def _compute_consistency(self):
+        results = self.cursor.execute('SELECT did, type, entity FROM entities')
+        tmp = results.fetchall()
+        df = DataFrame(tmp, columns=['did', 'e_type', 'entity'])
+        df = df.drop_duplicates()
+        tmp = df.groupby(['e_type', 'entity']).size().reset_index()
+        tmp.rename(columns={0: 'term_cons'}, inplace=True)
+
+        return tmp
+
     def compute_tf_idf(self):
         # Find total number of document
         results = self.cursor.execute('SELECT seq FROM sqlite_sequence WHERE name=\'{}\''.format('documents'))
         tmp = results.fetchone()
         total_doc = tmp[0]
 
+        results = self.cursor.execute('SELECT did, total_word, path FROM documents')
+        tmp = results.fetchall()
+        documents_df = DataFrame(tmp, columns=['did', 'total_word', 'path'])
+        documents_df['tf_idf'] = 0.0
+
+        no_docterm = {}
+
+        for query in self.queries:
+            no_docterm[query] = 0
+
+        for index, row in documents_df.iterrows():
+            path = row['path']
+            with codecs.open(path, 'rt') as f:
+                text = f.read()
+                for query in self.queries:
+                    if query in text:
+                        no_docterm[query] += 1
+
+        for query in self.queries:
+            for index, row in documents_df.iterrows():
+                total_word = row['total_word']
+                path = row['path']
+
+                with codecs.open(path, 'rt') as f:
+                    text = f.read()
+
+                tf_idf = self._compute_tf_idf(text, total_word, total_doc, no_docterm[query])
+                cur_tf_idf = documents_df.get_value(index, 'tf_idf')
+                documents_df.set_value(index, 'tf_idf', cur_tf_idf + tf_idf)
+
         results = self.cursor.execute('SELECT did, type, entity FROM entities')
         tmp = results.fetchall()
         df = DataFrame(tmp, columns=['did', 'e_type', 'entity'])
+        df['tf_idf'] = 0.0
 
-        base_df = df[['e_type', 'entity']]
-        base_df = base_df.drop_duplicates()
+        for index, row in df.iterrows():
+            did = row['did']
+            tf_idf = documents_df[documents_df['did'] == did]['tf_idf'].values[0]
+            df.set_value(index, 'tf_idf', tf_idf)
 
-        doc_t_df = df.drop_duplicates().groupby('entity').size()
+        del df['did']
+        df = df.groupby(['e_type', 'entity']).sum().reset_index()
+        return df
 
-        results = self.cursor.execute('SELECT did, total_word FROM documents')
-        tmp = results.fetchall()
-        df2 = DataFrame(tmp, columns=['did', 'total_word'])
+    def _compute_tf_idf(self, text, total_word, total_doc, no_docterm):
+        tf = self._compute_tf(text, total_word)
+        idf = self._compute_idf(total_doc, no_docterm)
+        return tf * idf
 
-        tmp = df[['did', 'entity']].groupby(['did', 'entity']).size().reset_index()
-        tmp.rename(columns={0: 'term_freq'}, inplace=True)
+    def _compute_tf(self, text, total_word):
+        term_freq = 0
+        for query in self.queries:
+            term_freq += text.count(query)
 
-        tf_idf_list = []
+        return float(term_freq) / total_word
 
-        for row in tmp.iterrows():
-            values = row[1]
-            did = values[0]
-            entity = values[1]
-            term_freq = values[2]
-            total_word = df2[df2['did'] == did]['total_word'].get_values()[0]
-            tf = float(term_freq) / total_word
-            doc_t = doc_t_df.get_value(entity)
-            idf = np.log(total_doc / doc_t)
-            tf_idf = tf * idf
-            tf_idf_list.append([entity, tf_idf])
+    def _compute_idf(self, total_doc, no_docterm):
+        return np.log(float(total_doc) / no_docterm)
 
-        tf_idf_df = DataFrame(tf_idf_list, columns=['entity', 'tf_idf'])
-        tf_idf_df = tf_idf_df.groupby('entity').agg('sum')
-
-        base_df.loc[:, 'tf_idf'] = base_df['entity'].apply(lambda x: tf_idf_df['tf_idf'][x])
-
-        return base_df
 
 if __name__ == '__main__':
-    a = Analyser('/Users/Sinderella/PycharmProjects/OSINT/db/20160124_203417')
+    a = Analyser('/Users/Sinderella/PycharmProjects/OSINT/db/20160124_203417', ['Thanat Sirithawornsant'])
     a.analyse()