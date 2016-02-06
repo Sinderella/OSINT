afbcaf99230c9e88a249ed6e04ba224a13e7bf1e
Sinderella <rawsec@gmail.com>
2016-02-06 16:39:59 +0000

    Added new table schema for keeping user input

--- schema.sql
+++ schema.sql
@@ -9,6 +9,12 @@ CREATE TABLE entity_types (
   type text UNIQUE
 );
 
+CREATE TABLE keywords (
+  type text,
+  keyword text UNIQUE,
+  FOREIGN KEY(type) REFERENCES entity_type(type)
+);
+
 CREATE TABLE entities (
   eid INTEGER PRIMARY KEY autoincrement NOT NULL,
   did INTEGER,
