889936d16ad37f940c49c628785c899b62d5571b
Sinderella <rawsec@gmail.com>
2016-02-29 10:13:52 +0000

    Ignore constraint db error

--- osint/utils/db_helpers.py
+++ osint/utils/db_helpers.py
@@ -4,11 +4,20 @@ import sqlite3
 def insert_keyword(cursor, keyword_type, keyword):
     if not isinstance(cursor, sqlite3.Cursor):
         return False
-    cursor.execute('INSERT INTO keywords(type, keyword) VALUES("{0}", "{1}")'.format(keyword_type, keyword))
+    try:
+        cursor.execute('INSERT INTO keywords(type, keyword) VALUES("{0}", "{1}")'.format(keyword_type, keyword))
+    except sqlite3.OperationalError as e:
+        print("Error inserting keyword: {0}\nKeyword Type: {1}\nKeyword: {2}".format(e.message, keyword_type, keyword))
 
 
 def insert_entity(cursor, did, entity, entity_type):
     if not isinstance(cursor, sqlite3.Cursor):
         return False
-    cursor.execute(
-        'INSERT INTO entities(did, entity, type) VALUES({0}, "{1}", "{2}")'.format(did, entity,  entity_type))
+    try:
+        cursor.execute(
+            'INSERT INTO entities(did, entity, type) VALUES({0}, "{1}", "{2}")'.format(did, entity, entity_type))
+    except sqlite3.OperationalError as e:
+        print(
+            "Error inserting entity: {0}\nDocument ID: {1}\nEntity Type: {2}\nEntity: {3}".format(e.message, did,
+                                                                                                  entity,
+                                                                                                  entity_type))
