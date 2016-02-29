13c53de0898e18f5ea2bed33b940ba9366ac144a
Sinderella <rawsec@gmail.com>
2016-02-29 10:11:40 +0000

    Changed analyser to thread
    - so it can be automated

--- osint/utils/analyser.py
+++ osint/utils/analyser.py
@@ -1,6 +1,7 @@
 import codecs
 import json
 import sqlite3
+from threading import Thread
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,15 +11,17 @@ from pandas import DataFrame, Series
 from osint.utils.requesters import Requester
 
 
-class Analyser(object):
+class Analyser(Thread):
     def __init__(self, db_name, queries=None, expected_types=None):
         super(Analyser, self).__init__()
         self.db_name = db_name
-        self.cur_db = sqlite3.connect(self.db_name + '/documents.db')
-        self.cursor = self.cur_db.cursor()
+        self.cur_db = None
+        self.cursor = None
 
         self.queries = queries
         self.expected_types = expected_types
+        if self.expected_types is None:
+            self.expected_types = ["location", "name", "organisation"]
 
         self.google_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address={}"
 
@@ -32,6 +35,11 @@ class Analyser(object):
             'type_5': ['political', 'administrative_area_level_2', 'sublocality_level_2'],
             'type_4': ['administrative_area_level_1', 'sublocality_level_1'],
             'type_3': ['country']}
+
+    def run(self):
+        self.cur_db = sqlite3.connect(self.db_name + '/documents.db')
+        self.cursor = self.cur_db.cursor()
+        self.analyse()
 
     def analyse(self):
         queries = []
@@ -61,6 +69,19 @@ class Analyser(object):
         print(self.get_category_top5(combined_df, 'tf_idf'))
         print(self.compute_summary(combined_df))
 
+        # Save as CSV file
+        csv_file = self.db_name + '_' + '_'.join(self.queries) + '.csv'
+        with codecs.open(csv_file, 'w') as f:
+            self.compute_summary(combined_df).to_csv(f, encoding='utf-8')
+
+        with codecs.open(csv_file, 'a') as f:
+            combined_df.sort_values(['e_type', 'tf_idf'], ascending=[True, False], ).to_csv(f, index=False,
+                                                                                            encoding='utf-8',
+                                                                                            columns=['e_type', 'entity',
+                                                                                                     'avg_score',
+                                                                                                     'consistency',
+                                                                                                     'ambiguity'])
+
     @staticmethod
     def normalise(src_df):
         column = src_df.columns[2]
@@ -73,6 +94,9 @@ class Analyser(object):
     @staticmethod
     def combine(src_df, dst_df):
         column = src_df.columns[2]
+        if column == 'tf_idf':
+            output = pd.merge(dst_df, src_df, on=["e_type", "entity"])
+            return output
         src_df['avg_score'] = src_df[column]
         output = pd.merge(dst_df, src_df, on=["e_type", "entity"])
         output['avg_score'] = output['avg_score_x'] + output['avg_score_y']
@@ -98,7 +122,7 @@ class Analyser(object):
         results = self.cursor.execute('SELECT type, entity FROM entities')
         tmp = results.fetchall()
         freq_df = DataFrame(tmp, columns=['e_type', 'entity'])
-        freq_df['ambiguity'] = 1
+        freq_df['ambiguity'] = 10
         freq_df = freq_df.drop_duplicates()
         result_computed_location = self._compute_location_ambiguity(freq_df)
         result_computed_name = self._compute_name_ambiguity(result_computed_location)
@@ -108,13 +132,15 @@ class Analyser(object):
         location_df = df[df['e_type'] == 'Location']
         location_se = location_df.drop_duplicates()['entity']
         for location_name in location_se.get_values():
-            url = self.google_api_url.format(location_name)
+            url = self.google_api_url.format(location_name.encode('utf-8'))
             req = Requester()
             res = req.get(url)
             api_result = json.loads(res.content)
             if api_result['status'] != 'OK':
                 continue
             location_types = api_result['results'][0]['address_components'][0]['types']
+            if not isinstance(location_types, list) or len(location_types) == 0:
+                continue
             location_score = max(self.__measure_location_ambiguity(location_type) for location_type in location_types)
             location_index = location_se[location_se == location_name].index[0]
             df.set_value(location_index, 'ambiguity', location_score)
@@ -233,7 +259,7 @@ class Analyser(object):
             with codecs.open(path, 'rt') as f:
                 text = f.read()
                 for query in self.queries:
-                    if query in text.decode('utf-8'):
+                    if query in text.decode('utf-8').lower():
                         no_docterm[query] += 1
 
         for query in self.queries:
@@ -277,35 +303,47 @@ class Analyser(object):
     def _compute_idf_queries(self, total_doc, no_docterm):
         return np.log(float(total_doc) / no_docterm)
 
-    def compute_noise(self):
+    def compute_completeness(self):
         result = self.cursor.execute('SELECT seq FROM sqlite_sequence WHERE name=\'{}\''.format('entities'))
         total_entity = result.fetchone()[0]
 
-        expected_type_total = 0.0
+        completeness = {}
         for expected_type in self.expected_types:
             result = self.cursor.execute(
                 'SELECT count(*) FROM entities WHERE type=\'{}\''.format(expected_type.title()))
-            expected_type_count = result.fetchone()[0]
-            expected_type_total += expected_type_count
+            expected_type_count = float(result.fetchone()[0])
+            completeness[expected_type + ' completeness'] = expected_type_count / total_entity
 
-        return 1 - (expected_type_total / total_entity)
+        return completeness
+
+    def compute_noise(self):
+        completeness = self.compute_completeness()
+        noise = {}
+        for expected_type in completeness:
+            noise[expected_type.replace('completeness', 'noise')] = 1 - completeness[expected_type]
+        return noise
 
     def compute_summary(self, combined_df):
         combined_mean = combined_df.mean()
         average_cons = combined_mean['consistency']
         average_ambi = combined_mean['ambiguity']
+        # completeness = self.compute_completeness()
         noise = self.compute_noise()
 
-        series = Series([average_cons, average_ambi, noise])
-        series.index = ['Average Consistency', 'Average Ambiguity', 'Noise']
+        series = Series([average_cons, average_ambi])
+        series.index = ['Average Consistency', 'Average Ambiguity']
 
+        series_2 = Series(noise)
+        series_2.index = [i.title() for i in series_2.index]
+        series = series.append(series_2)
         return series
 
 
 if __name__ == '__main__':
-    a = Analyser('/Users/Sinderella/PycharmProjects/OSINT/db/20160206_155954', ['Thanat Sirithawornsant'], ['location'])
+    a = Analyser('/Users/Sinderella/PycharmProjects/OSINT/db/20160221_124023')
     # plt.matplotlib.style.use('ggplot')
-    a.analyse()
+    a.start()
+    a.join()
     # ts = pd.merge(Analyser.normalise(a.compute_consistency()), Analyser.normalise(a.compute_frequency()))
     # ts = pd.merge(ts, Analyser.normalise(a.compute_tf_idf_queries()))
     # plt.scatter(ts['consistency'], ts['tf_idf'])
