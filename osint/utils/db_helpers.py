f31b1521468ba3d46b9dc920677693745b7a339c
Sinderella <rawsec@gmail.com>
2016-02-29 18:08:24 +0000

    Added more exceptions while inserting to db

--- osint/utils/db_helpers.py
+++ osint/utils/db_helpers.py
@@ -6,7 +6,7 @@ def insert_keyword(cursor, keyword_type, keyword):
         return False
     try:
         cursor.execute('INSERT INTO keywords(type, keyword) VALUES("{0}", "{1}")'.format(keyword_type, keyword))
-    except sqlite3.OperationalError as e:
+    except (sqlite3.OperationalError, ValueError) as e:
         print("Error inserting keyword: {0}\nKeyword Type: {1}\nKeyword: {2}".format(e.message, keyword_type, keyword))
 
 
@@ -16,7 +16,7 @@ def insert_entity(cursor, did, entity, entity_type):
     try:
         cursor.execute(
             'INSERT INTO entities(did, entity, type) VALUES({0}, "{1}", "{2}")'.format(did, entity, entity_type))
-    except sqlite3.OperationalError as e:
+    except (sqlite3.OperationalError, ValueError) as e:
         print(
             "Error inserting entity: {0}\nDocument ID: {1}\nEntity Type: {2}\nEntity: {3}".format(e.message, did,
                                                                                                   entity,
